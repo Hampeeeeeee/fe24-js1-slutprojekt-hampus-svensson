03216475c5deb3799c6cdab58cf01dd5618c5b72
Hampeeeeeee <hampus_svensson99@hotmail.com>
2024-12-27 19:43:09 +0100

    Third commit.
    Added some more css styling as well as a function for the searchbar which caused me more issues.

--- js/script.js
+++ js/script.js
@@ -12,43 +12,45 @@ const topTenPopularBtn = document.querySelector('#popularBtn')
 const topTenRatedBtn = document.querySelector('#ratedBtn');
 const popularMovieText = document.querySelector('#popularMovieText');
 const ratedMovieText = document.querySelector('#ratedMovieText');
+const personList = document.querySelector('#peopleDiv');
 
 const popularUrl = `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1`;
 const topRatedUrl = `https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1`;
+const personUrl = `https://api.themoviedb.org/3/search/person?include_adult=false&language=en-US&page=1`;
+const movieUrl = `https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1`;
 
 fetch(popularUrl, options)
-    .then(res => res.json())
+    .then(response => response.json())
     .then(json => {
         const popularUrl = json.results.slice(0, 10);
         console.log("Populära:", popularUrl);
     })
-    .catch(err => console.error('error:' + err));
+    .catch(error => console.error('error:' + error));
 
 fetch(topRatedUrl, options)
-    .then(res => res.json())
+    .then(response => response.json())
     .then(json => {
         const topRatedUrl = json.results.slice(0, 10);
         console.log("Högst betyg:", topRatedUrl);
     })
-    .catch(err => console.error('error:' + err));
-
+    .catch(error => console.error('error:' + error));
 
 topTenPopularBtn.addEventListener('click', () => {
 
     fetch(popularUrl, options)
-        .then(res => {
-            if (res.status >= 200 && res.status < 300) {
-                return res.json()
+        .then(response => {
+            if (response.status >= 200 && response.status < 300) {
+                return response.json()
             }
-            else if (res.status == 404) {
-                throw "The languge does not exist."
+            else if (response.status == 404) {
+                throw "That name does not exist."
             }
             else {
                 throw "Something whent wrong. Please try again later."
             }
         })
         .then(displayPopular)
-        .catch(err => console.error('Error: ' + err));
+        .catch(error => console.error('Error: ' + error));
 });
 
 function displayPopular(popularMovies) {
@@ -57,6 +59,7 @@ function displayPopular(popularMovies) {
     const popularMovieText = document.querySelector('#popularMovieText');
 
     popularMovieList.innerHTML = '';
+    personList.innerHTML = '';
 
     const top10popular = popularMovies.results.slice(0, 10);
     popularMovieText.classList.add('visible');
@@ -75,19 +78,19 @@ function displayPopular(popularMovies) {
 
 topTenRatedBtn.addEventListener('click', () => {
     fetch(topRatedUrl, options)
-        .then(res => {
-            if (res.status >= 200 && res.status < 300) {
-                return res.json()
+        .then(response => {
+            if (response.status >= 200 && response.status < 300) {
+                return response.json()
             }
-            else if (res.status == 404) {
-                throw "The languge does not exist."
+            else if (response.status == 404) {
+                throw "That name does not exist."
             }
             else {
                 throw "Something whent wrong. Please try again later."
             }
         })
         .then(displayRated)
-        .catch(err => console.error('Error: ' + err));
+        .catch(error => console.error('Error: ' + error));
 });
 
 function displayRated(ratedMovies) {
@@ -96,6 +99,7 @@ function displayRated(ratedMovies) {
     const ratedMovieText = document.querySelector('#ratedMovieText')
 
     ratedMovieList.innerHTML = '';
+    personList.innerHTML = '';
 
     const top10rated = ratedMovies.results.slice(0, 10);
     ratedMovieText.classList.add('visible');
@@ -117,5 +121,50 @@ searchbarForm.addEventListener('submit', handleSearchbarInput)
 function handleSearchbarInput(event) {
     event.preventDefault();
 
+    const userInput = searchbarForm.querySelector('input').value;
+    console.log(userInput);
 
+    
+    const personUrl = `https://api.themoviedb.org/3/search/person?query=${userInput}&include_adult=false&language=en-US&page=1`;
+    const movieUrl = `https://api.themoviedb.org/3/search/movie?query=${userInput}&include_adult=false&language=en-US&page=1`;
+
+    fetch(personUrl, options)
+    .then(response => response.json())
+    .then(json => {
+        const personUrl = json.results;
+        displayPeople(personUrl);
+    })
+    .catch(error => console.error('error:' + error));
+
+    fetch(movieUrl, options)
+    .then(response => response.json())
+    .then(json => {
+        const movieUrl = json.results;
+    })
+    .catch(error => console.error('error:' + error));
+}
+
+function displayPeople(people) {
+    //console.log(people);
+    const personList = document.querySelector('#peopleDiv');
+
+    personList.innerHTML = '';
+
+    console.log("Person:", people);
+
+    people.forEach(person => {
+        const profilePath = person.profile_path ? `https://image.tmdb.org/t/p/w200${person.profile_path}` : 'default-image-url.jpg';
+
+        const actorItem = document.createElement('div');
+        const known_for = person.known_for.map(known_for => {
+            return `<li>${known_for.original_title}</li>`
+        })  
+        console.log(known_for);
+        actorItem.innerHTML = `<h3>${person.name}</h3>
+                <img src="${profilePath}" alt="${person.name}"/>
+                <p>Known by: ${person.known_for_department}</p>
+                <h4>Known for: </h4>${known_for.join('')}    `;
+                    
+        personList.appendChild(actorItem);
+    })
 }
