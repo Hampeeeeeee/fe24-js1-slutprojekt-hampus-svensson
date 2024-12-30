4f120155fb0fde7a0560a30bd014b8ce678c656a
Hampeeeeeee <hampus_svensson99@hotmail.com>
2024-12-30 17:15:41 +0100

    Fifth session.
    Hair grew back so I added another function that displays movies/titles, as well as adding a bit more styling.

--- js/script.js
+++ js/script.js
@@ -58,14 +58,14 @@ topTenPopularBtn.addEventListener('click', () => {
 function displayPopular(popularMovies) {
     console.log(popularMovies);
     const popularMovieList = document.querySelector('#movieDiv');
-    const popularMovieText = document.querySelector('#popularMovieText');
+
+    topTenPopularBtn.classList.add('popularBtnActive');
+    topTenRatedBtn.classList.remove('ratedBtnActive');
 
     popularMovieList.innerHTML = '';
     personList.innerHTML = '';
 
     const top10popular = popularMovies.results.slice(0, 10);
-    popularMovieText.classList.add('visible');
-    ratedMovieText.classList.remove('visible');
 
     top10popular.forEach(movie => {
         const popularMovieItem = document.createElement('div');
@@ -98,14 +98,14 @@ topTenRatedBtn.addEventListener('click', () => {
 function displayRated(ratedMovies) {
     console.log(ratedMovies);
     const ratedMovieList = document.querySelector('#movieDiv');
-    const ratedMovieText = document.querySelector('#ratedMovieText')
+    
+    topTenRatedBtn.classList.add('ratedBtnActive');
+    topTenPopularBtn.classList.remove('popularBtnActive');
 
     ratedMovieList.innerHTML = '';
     personList.innerHTML = '';
-
+    
     const top10rated = ratedMovies.results.slice(0, 10);
-    ratedMovieText.classList.add('visible');
-    popularMovieText.classList.remove('visible');
 
     top10rated.forEach(movie => {
         const ratedMovieItem = document.createElement('div');
@@ -124,33 +124,38 @@ function handleSearchbarInput(event) {
     event.preventDefault();
 
     const userInput = searchbarForm.querySelector('input').value;
-    console.log(userInput);
+
+    topTenPopularBtn.classList.remove('popularBtnActive');
+    topTenRatedBtn.classList.remove('ratedBtnActive');
 
     popularMovieList.innerHTML = '';
+    popularMovieText.innerHTML = '';
+    ratedMovieList.innerHTML = '';
+    ratedMovieText.innerHTML = '';
 
     const personUrl = `https://api.themoviedb.org/3/search/person?query=${userInput}&include_adult=false&language=en-US&page=1`;
     const movieUrl = `https://api.themoviedb.org/3/search/movie?query=${userInput}&include_adult=false&language=en-US&page=1`;
 
-    fetch(personUrl, options)
-    .then(response => response.json())
-    .then(json => {
-        const personUrl = json.results;
-        displayPeople(personUrl);
-    })
-    .catch(error => console.error('error:' + error));
-
     fetch(movieUrl, options)
-    .then(response => response.json())
-    .then(json => {
-        const movieUrl = json.results;
-    })
-    .catch(error => console.error('error:' + error));
+        .then(response => response.json())
+        .then(json => {
+            const movieResults = json.results;
+            displayMovie(movieResults);
+        })
+        .catch(error => console.error('error:' + error));
+
+    fetch(personUrl, options)
+        .then(response => response.json())
+        .then(json => {
+            const personResults = json.results;
+            displayPeople(personResults);
+        })
+        .catch(error => console.error('error:' + error));
 
     searchbarForm.reset();
 }
 
 function displayPeople(people) {
-    //console.log(people);
     const personList = document.querySelector('#peopleDiv');
 
     personList.innerHTML = '';
@@ -162,14 +167,39 @@ function displayPeople(people) {
 
         const actorItem = document.createElement('div');
         const known_for = person.known_for.map(known_for => {
-            return `<li>${known_for.original_title}</li>`
-        })  
+            return `<li>${known_for.media_type.toUpperCase()}: ${known_for.original_title || known_for.name}</li>`
+        })
         console.log(known_for);
-        actorItem.innerHTML = `<h3>${person.name}</h3>
+        actorItem.innerHTML = `
+                <h2>People:</h2>
+                <h3>${person.name}</h3>
                 <img src="${profilePath}" alt="${person.name}"/>
-                <p>Known by: ${person.known_for_department}</p>
-                <h4>Known for: </h4>${known_for.join('')}    `;
-                    
+                <p>Profession: ${person.known_for_department}</p>
+                <h4>Known for:</h4> ${known_for.join('')}`;
+
         personList.appendChild(actorItem);
     })
+}
+
+function displayMovie(movies) {
+
+    const movieList = document.querySelector('#movieDiv');
+
+    movieList.innerHTML = '';
+
+    console.log('Movies:', movies)
+
+    movies.forEach(movie => {
+        const posterPath = movie.backdrop_path ? `https://image.tmdb.org/t/p/w200${movie.backdrop_path}` : './images/istockphoto-1409329028-612x612.jpg';
+
+        const movieItem = document.createElement('div');
+
+        movieItem.innerHTML = `<h2>Title:</h2>
+                <h3>${movie.title}</h3>
+                <img src="${posterPath}" alt="${movie.title}"/>
+                <p>Release date: ${movie.release_date}</p>
+                <h5>Description: ${movie.overview}</h5>`;
+
+        movieList.appendChild(movieItem);
+    });
 }
